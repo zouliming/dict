ce0b56a7c398e2e738e7aefac38a1fceeba637cc
zouliming <zouliming888@gmail.com>
2012-09-19 17:45:10 +0800

    修复了浏览器的兼容性

--- dict.php
+++ dict.php
@@ -3,15 +3,26 @@
 		<meta charset="UTF-8">
 		<title>葵花宝典</title>
 		<style>
+            body{
+                background-color: #222222;
+            }
 		    table{ border-collapse:collapse;border:1px solid #CCC;background:#efefef;width:100%; margin-bottom:1em;}  
-		    table th{ text-align:left; font-weight:bold; padding:.5em 2em .5em .75em; line-height:1.6em; font-size:12px; border:1px solid #CCC;}  
-		    table td{ padding:.5em .75em; line-height:1.6em; font-size:12px; border:1px solid #CCC;background-color:#fff;}
+		    table th{ text-align:left; font-weight:500; padding:.5em 2em .5em .75em; line-height:1.6em; font-size:11pt; border:1px solid #CCC;}  
+		    table td{ padding:8px; line-height:1.6em; font-size:14px; border:1px solid #CCC;background-color:#fff;}
 		    .c1{ width: 150px;}
 		    .c2{ width: 120px;}
 		    .c3{ width: 130px;}
 		    .c4{ width: 130px;}
 			.c5{ width: 230px;}
-		    caption{ font-size:14px; font-weight:bold; line-height:2em; text-align:left; }
+		    caption{
+                font-size:14px; 
+                font-weight:bold; 
+                line-height:2em; 
+                text-align:left; 
+                color: white;
+                font-family: Tahoma;
+                margin-bottom: 10px;
+            }
 		    table tbody tr:hover td, .table tbody tr:hover th {
 			    background-color: #F5F5F5;
 			}
@@ -21,8 +32,58 @@
             .tableComment{
                 display: inline-block;
                 height: 20px;
-                max-width: 400px;
+                /*max-width: 1000px;*/
                 min-width: 30px;
+            }
+            input[type='button']{
+                border: 0 none;
+                color: #FFFFFF;
+                font-family: Microsoft Yahei;
+                font-weight: 600;
+                line-height: 20px;
+                margin-right: 10px;
+                padding: 2px 8px;
+                cursor: pointer;
+            }
+            .button{
+                border: 0 none;
+                color: #FFFFFF;
+                font-family: "Microsoft Yahei","微软雅黑","Verdana", "Simsun", "Segoe UI", "Segoe UI Web Regular", "Segoe UI Symbol", "Helvetica Neue", "BBAlpha Sans", "S60 Sans", "Arial", "sans-serif";
+                font-weight: 600;
+                line-height: 20px;
+                margin-right: 10px;
+                padding: 4px 14px;
+                cursor: pointer;
+                font-size: 10pt;
+                position: relative;
+                display: inline-block;
+            }
+            .metro_btn_red{
+                background-color: #BC1C48;
+            }
+            .metro_btn_green{
+                background-color: #01A31C;
+            }
+            .metro_btn_blue{
+                background-color: #2D89F0;
+            }
+            .metro_text{
+                background: #D2D2D2;
+                border: 2px #D2D2D2 solid;
+                color: #646464;
+                border-radius: 0;
+                font-size: 11pt;
+                padding: 5px 8px;
+                outline: 0;
+                display: inline-block;
+                margin-right: 10px;
+            }
+            .tip {
+                background-color: #FF9900;
+                cursor: pointer;
+                height: 1.6em;
+                width: 30px;
+                float:left;
             }
 	    </style>
 	    <link href="css/tooltip.css" rel="stylesheet" type="text/css" />
@@ -53,7 +114,7 @@
 		<table>
 			<caption>
 				<A NAME="<?=$table?>"><?=$table?></A>
-                (<div id="<?='table__'.$table?>" class="tableComment"><?=@$diyComment['table'][strtolower($table)]?></div>)
+                (<span id="<?='table__'.$table?>" class="tableComment"><?=@$diyComment['table'][strtolower($table)]?></span>)
 			</caption>
 			<tbody>
 				<tr>
@@ -81,10 +142,10 @@
 					<td class="c6" id="<?=$table.'__'.$info['column_name']?>">
 						<?
 						if($line_show!="" && $config_show!=$line_show){
-							echo '<a class="tip" href="#" rel="tooltip" title="'.$line_show.'">差别</a>&nbsp;&nbsp;';
+                            echo '<div class="tip" href="#" rel="tooltip" title="'.$line_show.'"></div>&nbsp;&nbsp;';
 						}
                         if($config_show=="" && $content!=""){
-                            echo "<input class='useDbComment' type='button' value='使用数据库注释'><div></div>";
+                            echo "<a class='useDbComment button metro_btn_red'>使用数据库注释</a><div></div>";
                         }else{
                             echo "<div>".$config_show."</div>";
                         }
@@ -102,7 +163,7 @@
 		<script src="js/transition.js"></script>
 		<script src="js/tooltip.js"></script>
         <script type="text/javascript">
-	        $('a.tip').tooltip({
+	        $('div.tip').tooltip({
 		        placement:'bottom'
 		    });
             $('.useDbComment').bind('click',function(){
@@ -113,12 +174,12 @@
                     type:'POST',
                     url:'dict_edit.php',
                     data:'id='+obj+'&intro='+v+'&databases='+databases,
-                    success:function(msg){
-                        var output = eval('('+msg+')');
+                    dataType:'json',
+                    success:function(output){
                         if(output.error==true){
                             alert(output.errorMessage);
                         }else{
-                            $('#'+obj.table+'__'+obj.field).html(output.comment);
+                            $('#'+output.table+'__'+output.field).html(output.comment);
                         }
                     }
                 });
@@ -132,12 +193,12 @@
                     type:'POST',
                     url:'dict_edit.php',
                     data:'id='+id+'&intro='+$('#'+id).val()+'&databases='+databases,
-                    success:function(msg){
-                        var output = eval('('+msg+')');
+                    dataType:'json',
+                    success:function(output){
                         if(output.error==true){
                             alert(output.errorMessage);
                         }else{
-                            $('#'+obj.table+'__'+obj.field).html(output.comment);
+                            $('#'+output.table+'__'+output.field).html(output.comment);
                         }
                     }
                 });
@@ -151,8 +212,8 @@
                     type:'POST',
                     url:'dict_edit.php',
                     data:'id='+obj+'&intro='+$('#'+obj).val()+'&type=table'+'&databases='+databases,
-                    success:function(msg){
-                        var output = eval('('+msg+')');
+                    dataType:'json',
+                    success:function(output){
                         if(output.error==true){
                             alert(output.errorMessage);
                         }else{
@@ -167,14 +228,14 @@
                 var id = 'config__'+td.attr('id');
                 val = $.trim(ele.html());
                 td.children().hide();
-                ele.html('<input type="text" rel="'+val+'" value="'+val+'" id="'+id+'" size="30" /><input type="button" value="修改" onclick="save_intro(\''+id+'\')" /><input type="button" value="取消" onclick="cancel_intro(\''+id+'\')" />').show();
+                ele.html('<input class="metro_text" type="text" rel="'+val+'" value="'+val+'" id="'+id+'" size="30" /><a class="button metro_btn_green" onclick="save_intro(\''+id+'\')">修改</a><a class="button metro_btn_blue" onclick="cancel_intro(\''+id+'\')">取消</a>').show();
                 $("#"+id).focus();
             });
-            $("div.tableComment").bind('dblclick',function(){
+            $("span.tableComment").bind('dblclick',function(){
                 var ele = $(this);
                 var editId = 'edit__'+ele.attr('id');
                 val = $.trim(ele.html());
-                ele.html('<input type="text" rel="'+val+'" value="'+val+'" id="'+editId+'" size="30" /><input type="button" value="修改" onclick="save_table_intro(\''+editId+'\')" /><input type="button" value="取消" onclick="cancel_table_intro(\''+editId+'\')" />').show();
+                ele.html('<input class="metro_text" type="text" rel="'+val+'" value="'+val+'" id="'+editId+'" size="30" /><a class="button metro_btn_green" onclick="save_table_intro(\''+editId+'\')">修改</a><a class="button metro_btn_blue" onclick="cancel_table_intro(\''+editId+'\')">取消</a>').show();
                 $("#"+editId).focus();
             });            
         </script>
